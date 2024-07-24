523cfe4e8be2c5425f2000866129ff75c8eed598
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-07-23 18:28:45 -1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-window.addEventListener('load', function(){
-	console.log('Page has loaded');
-	document.getElementById('loading-page').style.display = 'none';
-	console.log('loading page hidden');
-	document.getElementById('main').style.display = 'block';
-	console.log('displaying content');
+window.addEventListener('load', function() {
+    console.log('Page has loaded');
+    document.getElementById('loading-page').style.display = 'none';
+    console.log('loading page hidden');
+    document.getElementById('main').style.display = 'block';
+    console.log('displaying content');
 });
-// ///////////////////////////////////////////////////////////////////////////////////////////
 
-window.addEventListener("scroll", function(){
-	var header = document.getElementById("cn-nav");
-	header.classList.toggle("cn-sticky", window.scrollY > 0);
-});
-// //////////////////////////////////////////////////////////////////////////////////////////////
+// Code for the loading page
+
+
+
+// code for on scroll intersection
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -20,10 +19,10 @@ const observer = new IntersectionObserver((entries, observer) => {
         }
     });
 });
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+// code for popup
 let popup = document.getElementById("popup");
 let banner = document.getElementById("cn-banner-text");
 
@@ -34,13 +33,34 @@ function openPopup() {
 
 }
 
-
 function closePopup() {
 	popup.classList.remove("open-popup");
 	banner.classList.remove("cn-banner-hidden");
 	document.body.style.overflow = "auto";
-}
+};
+//// code for sticky navbar
+    window.addEventListener('scroll', function() {
+      var navbar = document.querySelector('.navbar');
+      if (window.scrollY > 50) { 
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
 
+//// code for carousel slide
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            items: 1, 
+            loop: true, 
+            margin: 10, 
+            nav: false, 
+            dots: true, 
+            autoplay: true, 
+            autoplayTimeout: 3000, 
+            autoplayHoverPause: true 
+        });
+    });
 
 
 
